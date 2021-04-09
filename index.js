82694a0ec15532059474e20c647716dddeef8fe1
duarteicaros <icaroduartec@gmail.com>
2021-04-08 21:55:26 -0300

    Atualização com Barramento de Eventos e Consulta

--- index.js
+++ index.js
@@ -1,22 +1,39 @@
-const express = require ('express');
+const express = require('express');
 //const bodyParser = require('body-parser');
 const app = express();
 //app.use(bodyParser.json());
-app.use(express.json());//ao invés de usar o body-parser pode só escrever dessa forma agora
+app.use(express.json()); //ao invés de usar o body-parser pode só escrever dessa forma agora
 const lembretes = {};
 contador = 0;
-app.get ('/lembretes', (req, res, next) => {
+app.get('/lembretes', (req, res, next) => {
     res.send(lembretes);
 });
-app.put('/lembretes', (req, res, next) => {
+// Na aula de barreamento de eventos colocar isso
+const axios = require("axios");
+
+//app.put('/lembretes', (req, res, next) => {  --linha antiga do código, mudar para esse debaixo
+app.put("/lembretes", async (req, res) => {
     contador++;
-    const {texto} = req.body;
+    const {
+        texto
+    } = req.body;
     lembretes[contador] = {
-    contador, texto
+        contador,
+        texto
     }
-    res.status(201).send(lembretes[contador]);
+    //acrescentar essa linha depois da aula barreamento de eventos
+    await axios.post("http://localhost:10000/eventos", {
+        tipo: "LembreteCriado",
+        dados: {
+            contador,
+            texto,
+        },
     });
+    //daqui pra baixo, normal
+    res.status(201).send(lembretes[contador]);
+});
+
 
 app.listen(4000, () => {
-console.log('Lembretes. Porta 4000');
+    console.log('Lembretes. Porta 4000');
 });
